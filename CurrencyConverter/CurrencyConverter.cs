21bd18b79ef46ffefff4def58270347f9bdc1fe5
Mohammad Mokhtari <Mohammad.Mokhtari86@gmail.com>
2024-03-02 10:25:21 +0330

    Initial

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,87 @@
+﻿namespace Services;
+
+public interface ICurrencyConverter
+{
+    /// <summary>
+    /// Clears any prior configuration.
+    /// </summary>
+    void ClearConfiguration();
+    /// <summary>
+    /// Updates the configuration. Rates are inserted or replaced internally.
+    /// </summary>
+    void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates);
+
+    /// <summary>
+    /// Converts the specified amount to the desired currency.
+    /// </summary>
+    double Convert(string fromCurrency, string toCurrency, double amount);
+}
+
+
+
+public class CurrencyConverter : ICurrencyConverter
+{
+    private readonly IDictionary<string, IDictionary<string, double>> _rateGraph;
+    private static readonly object _grapLock = new();
+
+    public CurrencyConverter() => _rateGraph = new Dictionary<string, IDictionary<string, double>>();
+    public void ClearConfiguration()
+    {
+        lock (_grapLock)
+            _rateGraph.Clear();
+    }
+    public void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates)
+    {
+        lock (_grapLock)
+        {
+            foreach (var conversionRate in conversionRates)
+            {
+                string from = conversionRate.Item1;
+                string to = conversionRate.Item2;
+                double rate = conversionRate.Item3;
+
+                if (_rateGraph.TryGetValue(from, out var fromGraph))
+                    fromGraph[to] = rate;
+                else
+                    _rateGraph[from] = new Dictionary<string, double>() { { to, rate } };
+
+
+                if (_rateGraph.TryGetValue(to, out var toGraph))
+                    toGraph[from] = 1 / rate;
+                else
+                    _rateGraph[to] = new Dictionary<string, double>() { { from, 1 / rate } };
+            }
+        }
+    }
+    public double Convert(string fromCurrency, string toCurrency, double amount)
+    {
+
+        HashSet<string> visitedNodes = [];
+        Queue<Tuple<string, double>> nodeQueue = new();
+        nodeQueue.Enqueue(new Tuple<string, double>(fromCurrency, amount));
+        while (nodeQueue.Count > 0)
+        {
+            var (currentCurrency, currentAmount) = nodeQueue.Dequeue();
+            visitedNodes.Add(currentCurrency);
+
+            if (_rateGraph.TryGetValue((currentCurrency), out var neighbors))
+            {
+                foreach (var neighbor in neighbors)
+                {
+                    string neighborCurrency = neighbor.Key;
+                    double neighborRate = neighbor.Value;
+                    if (neighborCurrency == toCurrency)
+                        return currentAmount * neighborRate;
+
+                    if (!visitedNodes.TryGetValue(neighborCurrency, out _))
+                        nodeQueue.Enqueue(new Tuple<string, double>(neighborCurrency, currentAmount * neighborRate));
+                }
+            }
+        }
+        // maybe exception
+        // maybe use result pattern to return behavior => i Prefer this
+        throw new InvalidCastException($"We Are Not Supporting The Requested Conversion:{fromCurrency} to {toCurrency}");
+    }
+
+    
+}
---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Services;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static void logConvertOperation(string from, string to, double amount, double convrtedAmount) => Console.WriteLine($"Converting {amount} From {from} To {to} Return {convrtedAmount}");
+
+        var serviceProvider = new ServiceCollection().AddSingleton<ICurrencyConverter, CurrencyConverter>().BuildServiceProvider();
+        var converter = serviceProvider.GetRequiredService<ICurrencyConverter>();
+
+        IEnumerable<Tuple<string, string, double>> convertionRates = [new("USD", "CAD", 1.34), new("CAD", "GBP", 0.58), new("USD", "EUR", 0.86), new("IRR", "USD", 1.5), new("TR", "MK", 1.5)];
+
+        converter.ClearConfiguration();
+        converter.UpdateConfiguration(convertionRates);
+
+        try
+        {
+            double firstConvert = converter.Convert("USD", "CAD", 1500);
+            logConvertOperation("USD", "CAD", 1500, firstConvert);
+
+            double secondConvert = converter.Convert("GBP", "EUR", 1500);
+            logConvertOperation("GBP", "EUR", 1500, secondConvert);
+
+            double thirdConvert = converter.Convert("IRR", "EUR", 1500);
+            logConvertOperation("IRR", "EUR", 1500, thirdConvert);
+
+            double invalidConvert = converter.Convert("MK", "EUR", 1500);
+            logConvertOperation("MK", "EUR", 1500, invalidConvert);
+        }
+        catch (Exception e)
+        {
+
+            Console.WriteLine(e.Message);
+        }
+        Console.ReadLine();
+    }
+}
